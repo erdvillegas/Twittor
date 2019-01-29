f32f285824bb03c0003d4aabee116194b1741b2f
Erik Villegas <erdvillegas@gmail.com>
2019-01-29 01:09:35 -0600

    Actualizando ruta sw

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var url = window.location.href;
-var swLocation = "/Twittor/sw.js";
-
-if(url.includes('localhost')){
-    swLocation = "/sw.js";
-}
+var swLocation = '/Twittor/sw.js';
 
 if (navigator.serviceWorker) {
+
+    if (url.includes('localhost')) {
+        swLocation = '/sw.js';
+    }
     navigator.serviceWorker.register(swLocation);
 }
 
